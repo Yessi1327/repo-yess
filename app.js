f4f540e69fd1d4b7ea02cb2de097feac24a18ec6
yessi1327 <a01273574@tec.mx>
2025-02-20 22:16:19 -0600

    intro js/ variables y prompts :)

--- app.js
+++ app.js
@@ -1,71 +1,30 @@
-
-//Una aplicacion web es un sistenma distribuido, significa que esta regado y debes comunicarte con los servidores y archivos 
-//esto implica conectarte a un archivo que no este en tu entorno
-//
+/*Una aplicacion web es un sistenma distribuido, significa que esta regado y debes comunicarte con los servidores 
+y archivos esto implica conectarte a un archivo que no este en tu entorno*/
 
 //Para ejecutar en node se pone en la terminal node app.js
 
 //importa la libreria para manejar archivos require=import fs es la libreria de node 
 const file_system = require('fs');
 
-// escritura sincronico bloqueo el sistema y pregunta algo y estara bloqueado hasta que te responda
-// si no se ejecuta es por que no lo guarde 
-//Ahorita utiliza el Sync
-file_system.writeFileSync("hola.txt","hola desde node")
-
-// pero el asincronico no bloquea el sistema y por default node es asi 
-// El codigo asincronico no se ejecuta en orden 
-//Tiene una fila de eventos 
-
-//Algoritmo de ordenamiento, codigo asincrono
-
-setTimeout(() => { console.log("jojo te hackie")
-    
-}, 10000);
-
-const arreglo = [5000,60,90,100,10,20,10000,0,120,2000,340,1000,50];
-//No se ejecuta en oreden si no que ,se ejecuta cuando tu le dices por que los imprime en funcion del tiempo del valor del arreglo
-//si hay dos elementos que se deban imprimir al mismo tiempo se hara por orden de fila
-for ( let item of arreglo){
-setTimeout(() => { console.log(item);
-    
-}, item);
-}
-
-//escucha el puerto 3000
-//localhost 3000
-// 127.0.01 3000
-// home: 3000
-
-const http = require("http");
-
-//Ahora si vamos a meter una pagina web de la foma correcta. 
-
-//Para meter el html
-const html = file_system.readFileSync ('index.html');
-
-//Construccion de la app.web
-const server =  http.createServer( (request, response) => {
-    console.log (request.url);
-    response.setHeader("Content-type", "text/html");
-    response.write(html);
-    response.end();
+/* Escritura sincronico bloqueo el sistema y pregunta algo y estara bloqueado hasta que te responda
+/ Si no se ejecuta es por que no lo guarde, Ahorita utiliza el Sync*/
+    file_system.writeFileSync("hola.txt","hola desde node")
 
 
-});
+/* Pero el asincronico no bloquea el sistema y por default node es asi.
+El codigo asincronico no se ejecuta en orden Tiene una fila de eventos*/
 
-//Para meter el html
-//const html = "Puedes poner aqui todo el HTML Y HARDCODEAR, Pero seria un codigo espageti"
+//Algoritmo de ordenamiento, codigo asincronico
 
-//Construccion de la app.web
-/*const server =  http.createServer( (request, response) => {
-    console.log (request.url);
-    response.setHeader("Content-type", "text/html");
-    response.write(html);
-    response.end();
+    setTimeout(() => { console.log("jojo te hackie") }, 10000);
 
+    const arreglo = [5000,60,90,100,10,20,10000,0,120,2000,340,1000,50];
+    /*No se ejecuta en oreden si no que ,se ejecuta cuando tu le dices por que los imprime en funcion del tiempo y del valor 
+    del arreglo si hay dos elementos que se deban imprimir al mismo tiempo se hara por orden de fila*/
+    for ( let item of arreglo){
 
-});*/
+     setTimeout(() => { console.log(item);}, item);
+    }
 
 
 //Construccion de la app.web
@@ -75,13 +34,50 @@ const server =  http.createServer( (request, response) => {
     response.write("<h2>Holi yessi</h2>");
     response.end();
 
+});*/
+
+//Para meter el html
+//const html = "Puedes poner aqui todo el HTML Y HARDCODEAR, Pero seria un codigo espageti"
+
+/*const server =  http.createServer( (request, response) => {
+    console.log (request.url);
+    response.setHeader("Content-type", "text/html");
+    response.write(html);
+    response.end();
+
 
 });*/
 
 
-server.listen (3000);
+//Esta linea igual es para añadir otra libreria
+const http = require("http");
 
-//siempre que haces una peticion al navegador el hace otra para pedir el icono
+//=Problema 6=
 
-//Para mandar una solicitud tienes que poner su ip de su servidor 
+    //Ahora si vamos a meter una pagina web de la foma correcta. El css no va funcionar por que eso lo aprenderemos despues 
+
+    //Para meter el html
+    const html = file_system.readFileSync ('index.html');
+
+    //Construccion de la app.web
+    const server =  http.createServer( (request, response) => {
+        console.log (request.url);
+        response.setHeader("Content-type", "text/html");
+        response.write(html);
+        response.end();
+
+    });
+
+    //escucha el puerto 3000
+    //localhost 3000
+    // 127.0.01 3000
+    // home: 3000
+    server.listen (3000);
+
+    /*Siempre que haces una peticion al navegador el hace otra para pedir el icono
+    Para mandar una solicitud tienes que poner su ip de su servidor*/
+
+// =Problema 1=
+
+
 
---
+++ jar/introjs.js
@@ -0,0 +1,59 @@
+// consola (log, info, warn, error, assert)
+
+// función que se usa para imprimir mensajes en la consola del navegador
+//Ayuda a verificar valores de variables y objetos
+//Permite entender cómo se están ejecutando las funciones
+//Mensajes informativos
+console.log("Aquí se publica los laboratorios");
+
+let nombre =  "Yessica";
+console.log("El nombre es:", nombre);
+
+//Igual imprimen mensajes en la consola.
+//Generalmente se muestra con un ícono de información
+console.info("Los laboratorios sirven para crear competencias");
+
+//Para advertir sobre el uso de funciones obsoletas 
+//Para alertar sobre valores inesperados o posibles errores
+//Para resaltar problemas que podrían causar fallos
+console.warn("Cuidado con hacer los laboratorios tarde jeje");
+
+//Mensajes de error graves.
+console.error("No se cargo el laboratorio aahhh :/");
+
+console.assert(1 == 1);
+//Esto da error
+//console.assert(1 === "1"); //tipo
+console.assert(1 == "1"); //valor
+console.assert(1 == true);
+//Esto da error
+//console.assert(1 === true);
+
+// variables, constantes
+
+//Antigua forma de declarar variables
+var dinosaurio = "T-Rex";
+console.log(dinosaurio)
+
+//Nueva forma de declarar variables, solo viene en el ámbito declarado
+let felino = "Tigre";
+
+//Para que no cambie
+const precio= 20.00;
+
+// Alcance de las variables
+// cuando se acabe el ciclo, la variable i muere entonces no se 
+// puede ejecutar fuera del ciclo
+for (let i = 0; i <= 10; i++){
+    
+    console.log(i); 
+}
+
+ //da error por que i no existe fuera del for
+//console.log(i); 
+
+//Manda una alerta al iniciar la pagina
+alert("En esta pagina accederas a mucho conocimiento, cuidado ;o");
+
+//Manda una pregunta con un campo a responder 
+const pregunta = prompt("Cual es mi animal favorito?");
