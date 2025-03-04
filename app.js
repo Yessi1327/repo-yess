7fd07ac2d4aa1e094474792e61b9dc78c9917827
yessi1327 <a01273574@tec.mx>
2025-03-03 21:44:04 -0600

    Creacion de pagina 404

--- app.js
+++ app.js
@@ -16,35 +16,21 @@ app.set('views', 'views');
 // Aqui configure que utilice el body parser en vez de use
 app.use(bodyParser.urlencoded({extended: false}));
 
-//express funciona atravez de middleware usando use y recibe tres 
-//parametros request: peticion response: respuesta, hay gente que le pone req y resp
-//next lo que hace es pasar al siguiente midelware
-
-//PARA QUE importar un modulo
+//PARA que importar un modulo
 const zooRoutes = require("./routes/zoo.routes");
 //PARA QUE el router se lo cuelgas a las demas rutas
 app.use('/zoo', zooRoutes);
 
-
-// cualquier otra ruta entra esta por que esta es la global
-app.use((request, response, next) => {
-    console.log('Middleware!');
-    next(); //Le permite a la petición avanzar hacia el siguiente middleware
-});
-
+// 404 tiene que ser tu ultimo midelware
 // send es la combinacion de las tres lineas el set header el write y el end 
 app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    response.send('¡Hola mundo!'); //Manda la respuesta
+    console.log('Error 404!');
+    //response.statusCode= 404; 
+    response.render('error_404');//Manda la respuesta
 });
 
-/*//Forma tradicional sin usar includes
-app.use("/plantas/agregar",(request, response, next)=> {
-    response.send(html_header+html_form+html_footer);
-});*/
-
 console.log(__dirname)
-// 404 tiene que ser tu ultimo midelware
+
 
 //escucha por el puerto 3000
 app.listen(3000);
