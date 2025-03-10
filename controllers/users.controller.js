6d3d6862a74c76ba3e79e3a54900a48ca7d72d72
yessi1327 <a01273574@tec.mx>
2025-03-10 12:28:49 -0600

    Mensaje de usuario creado

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,10 +1,16 @@
 const Usuario = require('../models/users.model');
 
 exports.get_signup = (request, response, next) => {
+    const mensaje = request.session.info || '';
+    if (request.session.info) {
+        request.session.info = '';
+    }
+    
     response.render("login.ejs",{
     isLoggedIn: request.session.isLoggedIn || false,
     username: request.session.username || '',
     isNew: true,
+    info: mensaje,
     });
 };
 
@@ -12,6 +18,7 @@ exports.post_signup = (request, response, next) =>{
     const usuario = new 
         Usuario(request.body.username, request.body.password);
     usuario.save().then(() => {
+        request.session.info = `Tu usuario se ha creado`;
         response.redirect('/users/login');
     }).catch((error) => {
         console.log(error);
@@ -19,12 +26,17 @@ exports.post_signup = (request, response, next) =>{
 };
 
 exports.get_login = (request, response, next) => {
+    const mensaje = request.session.info || '';
+    if (request.session.info) {
+        request.session.info = '';
+    }
     
     response.render('login.ejs', {
         //QUE HACE
         isLoggedIn: request.session.isLoggedIn || false,
         username: request.session.username || '',
         isNew: false,
+        info: mensaje,
     });
 };
 
